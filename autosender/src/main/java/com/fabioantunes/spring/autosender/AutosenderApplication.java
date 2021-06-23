3f0d00b59378dae9137d4bfec7cac9ea8d2f3ad2
fabioexe <fabio.avasc@gmail.com>
2021-06-23 16:13:36 -0300

    Inicio do projeto

---
+++ autosender/src/main/java/com/fabioantunes/spring/autosender/AutosenderApplication.java
@@ -0,0 +1,13 @@
+package com.fabioantunes.spring.autosender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutosenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutosenderApplication.class, args);
+	}
+
+}
---
+++ autosender/src/test/java/com/fabioantunes/spring/autosender/AutosenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabioantunes.spring.autosender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutosenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
